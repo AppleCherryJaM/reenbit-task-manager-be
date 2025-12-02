c38d00cfd574693b13de93510db01667e7ff29f3
Max Bolsunovsky <jamax.cherry@gmail.com>
2025-12-02 15:48:49 +0200

    Added basic CORS settings

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import * as dotenv from "dotenv";
+import cors from 'cors';
 
 dotenv.config();
 
@@ -17,6 +18,11 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(cors({
+  origin: process.env.CORS_FRONTEND_URL,
+  credentials: true,
+}));
 
 app.use("/api", router);
 
