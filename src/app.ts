c72b41746de5a6907a1694214f6693421318c161
Max Bolsunovsky <jamax.cherry@gmail.com>
2025-12-15 14:05:19 +0200

    Fixing cors error

--- src/app.ts
+++ src/app.ts
@@ -15,14 +15,26 @@ import router from "./routes/main-router";
 
 const app: Application = express();
 const PORT = process.env.PORT;
-const FE_URL = process.env.CORS_FRONTEND_URL;
+const allowedOrigins = (process.env.CORS_FRONTEND_URL || "")
+  .split(",")
+  .map(url => url.trim())
+  .filter(url => url.length > 0);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(
 	cors({
-		origin: FE_URL,
+		origin: (origin, callback) => {
+
+      if (!origin) return callback(null, true);
+      
+			if (allowedOrigins.includes(origin)) {
+        return callback(null, origin);
+      }
+			
+      return callback(new Error("Not allowed by CORS"));
+    },
 		credentials: true,
 		methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
 		allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
