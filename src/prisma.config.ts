099811cb2685bd8710fa4f7b02e6b4a1ac62bea3
Max Bolsunovsky <jamax.cherry@gmail.com>
2025-12-15 15:26:15 +0200

    Providing new error for prisma

--- src/prisma.config.ts
+++ src/prisma.config.ts
@@ -1,6 +1,10 @@
 import { defineConfig } from "prisma/config";
 
-const dbUrl = process.env.DATABASE_URL || "";
+const dbUrl = process.env.DATABASE_URL;
+
+if (!dbUrl) {
+  throw new Error("DATABASE_URL is not set in environment variables.");
+}
 
 export default defineConfig({
 	schema: "prisma/schema.prisma",
